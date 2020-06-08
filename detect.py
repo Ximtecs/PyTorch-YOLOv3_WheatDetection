aa7d8b79309504420082cf5f0c797f4f5767effe
ximtecs <michael.haahr101293@gmail.com>
2020-06-08 22:07:07 +0200

    Added NaN error detection in detect.py

--- detect.py
+++ detect.py
@@ -23,12 +23,12 @@ from matplotlib.ticker import NullLocator
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--image_folder", type=str, default="data/samples", help="path to dataset")
+    parser.add_argument("--image_folder", type=str, default="data/samples/test", help="path to dataset")
     parser.add_argument("--model_def", type=str, default="config/yolov3-Wheat-tiny.cfg", help="path to model definition file")
     parser.add_argument("--weights_path", type=str, default="checkpoints/yolov3_ckpt_50.pth", help="path to weights file")
     parser.add_argument("--class_path", type=str, default="data/WheatDetection/classes.names", help="path to class label file")
-    parser.add_argument("--conf_thres", type=float, default=0.7, help="object confidence threshold")
-    parser.add_argument("--nms_thres", type=float, default=0.5, help="iou thresshold for non-maximum suppression")
+    parser.add_argument("--conf_thres", type=float, default=0.8, help="object confidence threshold")
+    parser.add_argument("--nms_thres", type=float, default=0.2, help="iou thresshold for non-maximum suppression")
     parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
     parser.add_argument("--n_cpu", type=int, default=0, help="number of cpu threads to use during batch generation")
     parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
@@ -48,7 +48,6 @@ if __name__ == "__main__":
         model.load_darknet_weights(opt.weights_path)
     else:
         # Load checkpoint weights
-        print("asdfasdjfhasljkdfhakjsldfhkjasdhkjahsdf")
         model.load_state_dict(torch.load(opt.weights_path))
 
     model.eval()  # Set in evaluation mode
@@ -118,22 +117,22 @@ if __name__ == "__main__":
 
                 box_w = x2 - x1
                 box_h = y2 - y1
-
-                out_string += f'{conf} {int(x1)} {int(y1)} {int(box_w)} {int(box_h)} '
-                color = bbox_colors[int(np.where(unique_labels == int(cls_pred))[0])]
-                # Create a Rectangle patch
-                bbox = patches.Rectangle((x1, y1), box_w, box_h, linewidth=2, edgecolor=color, facecolor="none")
-                # Add the bbox to the plot
-                ax.add_patch(bbox)
-                # Add label
-                plt.text(
-                    x1,
-                    y1,
-                    s=classes[int(cls_pred)],
-                    color="white",
-                    verticalalignment="top",
-                    bbox={"color": color, "pad": 0},
-                )
+                if not(np.isnan(x1) and np.isnan(y1) and  np.isnan(box_w) and np.isnan(box_h)): 
+                    out_string += f'{conf} {int(x1)} {int(y1)} {int(box_w)} {int(box_h)} '
+                    color = bbox_colors[int(np.where(unique_labels == int(cls_pred))[0])]
+                    # Create a Rectangle patch
+                    bbox = patches.Rectangle((x1, y1), box_w, box_h, linewidth=2, edgecolor=color, facecolor="none")
+                    # Add the bbox to the plot
+                    ax.add_patch(bbox)
+                    # Add label
+                    plt.text(
+                        x1,
+                        y1,
+                        s=classes[int(cls_pred)],
+                        color="white",
+                        verticalalignment="top",
+                        bbox={"color": color, "pad": 0},
+                    )
 
 
         # Save generated image with detections
